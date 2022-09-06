3975baa74b95995cc0faa9cf458012ebface733a
MccGrey <chuxzzie01@yahoo.com>
2022-09-06 08:21:18 +0100

    tasks on array

--- script.js
+++ script.js
@@ -876,8 +876,8 @@ const data2 = [12, 5, -5, 0, 4];
 const printForecast = function (arr) {
   let str = "";
   for (let i = 0; i < arr.length; i++) {
-    str = str + `${arr[i]}'C`;
+    str = str + `${arr[i]}'C in ${i + 1} days ... `;
   }
-  console.log(str);
+  console.log("..." + str);
 };
 printForecast(data1);
